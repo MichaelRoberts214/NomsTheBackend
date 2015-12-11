bf470b89d8ef3787ac785944f253cfa46e38f5e1
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-12-10 22:35:21 -0800

    create restaurant schema

---
+++ app/models/restaurant.js
@@ -0,0 +1,10 @@
+// app/models/bear.js
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var RestaurantSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);

0fccc89b741217513867f12e8914d406e6bc7e53
MichaelRoberts214 <m_roberts6@u.pacific.edu>
2015-12-10 22:19:41 -0800

    code express server

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-//
+var express    = require('express');
+var bodyParser = require('body-parser');
+
+var app        = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// API ROUTES
+var router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+  res.json({ message: 'Welcome to noms api' });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER ROUTES
+// all routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+app.listen(port);
+console.log('Magic happens on port ' + port);
